919e5bd38ffefae5de4a8a4f55ff49c66145a9b7
Hristo Petkov <hpe@pce-instruments.com>
2024-09-08 00:07:15 +0300

    implement footer

--- lib/utils/image_utils.dart
+++ lib/utils/image_utils.dart
@@ -1,4 +1,8 @@
 class ImageUtils {
   static const marketinyaLabelPath = 'assets/marketinya-label.svg';
   static const marketinyaLogoPath = 'assets/marketinya-logo.svg';
+  static const facebookLogo = 'assets/facebook-logo.svg';
+  static const instagramLogo = 'assets/instagram-logo.svg';
+  static const linkedinLogo = 'assets/linkedin-logo.svg';
+  static const xLogo = 'assets/x-logo.svg';
 }
