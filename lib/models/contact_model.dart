c6e212c907ef64340dfa4e457fba52a33446e342
Hristo Petkov <hpe@pce-instruments.com>
2024-09-14 00:37:12 +0300

    save questions to firebase

---
+++ lib/models/contact_model.dart
@@ -0,0 +1,33 @@
+class ContactModel {
+  String name;
+  String telephone;
+  String firmName;
+  String email;
+  String question;
+  String date;
+
+  ContactModel(
+    this.name,
+    this.telephone,
+    this.firmName,
+    this.email,
+    this.question,
+    this.date,
+  );
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'telephone': telephone,
+      'firmName': firmName,
+      'email': email,
+      'question': question,
+      'date': date,
+    };
+  }
+
+  @override
+  String toString() {
+    return 'ContactModel{name: $name, telephone: $telephone, firmName: $firmName, email: $email, question: $question, date: $date}';
+  }
+}
