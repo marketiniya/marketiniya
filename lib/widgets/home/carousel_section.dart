b3b3b90fa4f8878553fc54f93eb18db7c10150ef
Hristo Petkov <hristo.petkov@tide.co>
2024-11-02 10:16:15 +0200

    carousel improvements

--- lib/widgets/home/carousel_section.dart
+++ lib/widgets/home/carousel_section.dart
@@ -16,13 +16,15 @@ class _CarouselSectionState extends State<CarouselSection> {
   final _carouselController = CarouselSliderController();
 
   final List<String> images = [
-    'assets/carousel/image0.png',
-    'assets/carousel/image1.png',
-    'assets/carousel/image2.png',
+    ImageUtils.carouselItemImage0,
+    ImageUtils.carouselItemImage1,
+    ImageUtils.carouselItemImage2,
   ];
 
   @override
   Widget build(BuildContext context) {
+    double maxCarouselWidth = 1500.0;
+
     return Container(
       width: double.infinity,
       decoration: const BoxDecoration(
@@ -35,51 +37,69 @@ class _CarouselSectionState extends State<CarouselSection> {
           ],
         ),
       ),
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          CarouselSlider.builder(
-            carouselController: _carouselController,
-            options: CarouselOptions(
-              height: MediaQuery.of(context).size.height * 0.75,
-              autoPlay: false,
-              enableInfiniteScroll: false,
-              initialPage: 1,
-              enlargeCenterPage: true,
-              viewportFraction: 0.3,
-            ),
-            itemCount: images.length,
-            itemBuilder: (BuildContext context, int index, int realIndex) {
-              return Image.asset(images[index]);
-            },
-          ),
-          Positioned(
-            left: 120,
-            child: GestureDetector(
-              onTap: () {
-                _carouselController.previousPage();
-              },
-              child: SvgPicture.asset(
-                ImageUtils.previousImagePath,
-                width: 40,
-                height: 72,
+      child: Center(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(maxWidth: maxCarouselWidth),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              CarouselSlider.builder(
+                carouselController: _carouselController,
+                options: CarouselOptions(
+                  height: MediaQuery.of(context).size.height * 0.75,
+                  autoPlay: false,
+                  enableInfiniteScroll: true,
+                  initialPage: 1,
+                  enlargeCenterPage: true,
+                  viewportFraction: 0.3,
+                  enlargeStrategy: CenterPageEnlargeStrategy.height,
+                  scrollPhysics: const BouncingScrollPhysics(),
+                ),
+                itemCount: images.length,
+                itemBuilder: (BuildContext context, int index, int realIndex) {
+                  return Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Image.asset(
+                      images[index],
+                      fit: BoxFit.cover,
+                    ),
+                  );
+                },
               ),
-            ),
-          ),
-          Positioned(
-            right: 120,
-            child: GestureDetector(
-              onTap: () {
-                _carouselController.nextPage();
-              },
-              child: SvgPicture.asset(
-                ImageUtils.nextImagePath,
-                width: 40, // Adjust the size as needed
-                height: 72,
+              Positioned(
+                left: 20,
+                child: GestureDetector(
+                  onTap: () {
+                    _carouselController.previousPage(
+                      duration: const Duration(milliseconds: 300),
+                      curve: Curves.easeInOut,
+                    );
+                  },
+                  child: SvgPicture.asset(
+                    ImageUtils.previousImagePath,
+                    width: 40,
+                    height: 72,
+                  ),
+                ),
               ),
-            ),
+              Positioned(
+                right: 20,
+                child: GestureDetector(
+                  onTap: () {
+                    _carouselController.nextPage(
+                      duration: const Duration(milliseconds: 300),
+                      curve: Curves.easeInOut,
+                    );
+                  },
+                  child: SvgPicture.asset(ImageUtils.nextImagePath,
+                    width: 40,
+                    height: 72,
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
