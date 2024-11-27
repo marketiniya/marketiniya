8472d3d2b95da285d65f3f3344dc249daa14c8be
Hristo Petkov <hristo.petkov@tide.co>
2024-11-27 19:38:04 +0200

    implement carousel_section section for mobile

--- lib/screens/home/layouts/home_mobile_layout.dart
+++ lib/screens/home/layouts/home_mobile_layout.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:marketinya/widgets/appBar/custom_app_bar_mobile.dart';
 import 'package:marketinya/widgets/home/mobile/business_overview_section_mobile.dart';
+import 'package:marketinya/widgets/home/mobile/carousel_section_mobile.dart';
+
+import '../../../utils/color_utils.dart';
 
 class HomeMobileLayout extends StatelessWidget {
   const HomeMobileLayout({super.key});
@@ -9,11 +13,15 @@ class HomeMobileLayout extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       appBar: CustomAppBarMobile(activeTab: 'Начало'),
-      body: Column(
-        children: [
-          BusinessOverviewSectionMobile(),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            BusinessOverviewSectionMobile(),
+            CarouselSectionMobile(),
+          ],
+        ),
       ),
     );
   }
+
 }
