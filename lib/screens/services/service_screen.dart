549a4ca332197b9d502fba6c054e24ae1742c0da
Hristo Petkov <hristo.petkov@tide.co>
2024-12-01 20:49:59 +0200

    implement services tab for mobile

---
+++ lib/screens/services/service_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:marketinya/config/responsive_layout.dart';
+import 'package:marketinya/screens/services/layout/service_desktop_layout.dart';
+import 'package:marketinya/screens/services/layout/service_mobile_layout.dart';
+
+
+class ServiceScreen extends StatelessWidget {
+  const ServiceScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ResponsiveLayout(
+      mobile: ServiceMobileLayout(),
+      desktop: ServiceDesktopLayout(),
+    );
+  }
+}
