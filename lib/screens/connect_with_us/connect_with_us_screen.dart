10b5d6aa12817561791bea0bae01d264424a4da3
Hristo Petkov <hristo.petkov@tide.co>
2024-12-01 21:03:29 +0200

    implement connect us tab for mobile

---
+++ lib/screens/connect_with_us/connect_with_us_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:marketinya/config/responsive_layout.dart';
+import 'package:marketinya/screens/connect_with_us/layouts/connect_with_us_desktop_layout.dart';
+import 'package:marketinya/screens/connect_with_us/layouts/connect_with_us_mobile_layout.dart';
+
+class ConnectWithUsScreen extends StatelessWidget {
+  const ConnectWithUsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ResponsiveLayout(
+      mobile: ConnectWithUsMobileLayout(),
+      desktop: ConnectWithUsDesktopLayout(),
+    );
+  }
+}
